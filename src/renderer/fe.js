d61b3ef8fea9fc4d15f1af0f65b420cd6b79520c
Hum4n01d <hum4n01d@icloud.com>
2017-04-10 12:59:26 -0700

    Deleted file for testing

--- src/renderer/fe.js
+++
@@ -1,8 +0,0 @@
-const {join} = require('path')
-const {readFileSync} = require('fs')
-
-const savePath = join(__dirname, "save/tabs.json")
-
-const initalTabs = JSON.parse(readFileSync(savePath).toString()).tabs
-
-console.log(initalTabs)
