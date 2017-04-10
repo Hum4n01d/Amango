c8349e28c4e03757408447f6c9757a9bdc2fdd47
Hum4n01d <hum4n01d@icloud.com>
2017-04-10 16:49:06 -0700

    Unignored vscode folder, stop opening devtools, and new color scheme!

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ function createWindow() {
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
     
   })
   mainWindow.on('closed', function () {
